492832b1666e09b9a2c70c757d3e39fdb9d19198
Richard Pineo <Richard.E.Pineo@ge.com>
2018-08-24 12:52:13 -0700

    forgotten

---
+++ Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using PineoTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace pineo_test_api
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<HelloWorldContext>(opt =>
+                opt.UseInMemoryDatabase("HelloWorld"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.Configure<ApiBehaviorOptions>(options =>
+                  {
+                      options.SuppressConsumesConstraintForFormFileParameters = true;
+                      options.SuppressInferBindingSourcesForParameters = true;
+                      options.SuppressModelStateInvalidFilter = true;
+                  });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
