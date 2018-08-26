b8e60c83cc9a172f27ffa444e3b6cc31688b13c5
Richard Pineo <Richard.E.Pineo@ge.com>
2018-08-26 12:25:34 -0700

    force change

--- Models/HelloWorldItem.cs
+++ Models/HelloWorldItem.cs
@@ -5,6 +5,7 @@ namespace PineoTest.Models
         public HelloWorldItem() {
             this.Salutation = "Hello, world.";
         }
+        
         public string Salutation { get; set; }
         public long Id {get; set;}
     }
